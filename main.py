f4e5719559a413cb28536b28de7bebea0acf8013
user-dam <dam62@dam62>
2025-03-26 19:13:08 +0100

    ui

---
+++ main.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "Mi primera aplicación con Flet"
+    page.add(ft.Text("¡Mi primer proyecto!"))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    ft.app(target=main, view=ft.WEB_BROWSER, port=8080)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
